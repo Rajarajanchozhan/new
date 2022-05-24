efb4612b7dc46531bdcb11c5c0e6753d827a3bec
raja <cholan299@gmail.com>
2022-05-24 17:58:58 +0530

    master

--- src/org/info/BasicClass.java
+++ src/org/info/BasicClass.java
@@ -4,5 +4,10 @@ public class BasicClass {
 public static void main(String[] args) {
 	System.out.println("monday");
 	System.out.println("tusaday");
+	
+	
+	System.out.println("fhfhfh");
+	System.out.println("cncnvn");
+	
 }
 }
